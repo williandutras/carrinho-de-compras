c844f603ca5b5e009db8e26c7f16dc7b4e7faf9f
Willian Dutras <willian_dutras@hotmail.com>
2017-06-23 15:57:51 -0300

    Formatando strings e valores

--- js/main.js
+++ js/main.js
@@ -12,14 +12,30 @@ function getTotal(list){
 	return total;
 }
 
+//criando a tabela
 function setList(list){
 	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 	for(var key in list){
-		table += '<tr><td>'+ list[key].desc +'</td><td>'+ list[key].amount +'</td><td>'+ list[key].value +'</td><td>Edit | Delete</td></tr>';
+		table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td>Edit | Delete</td></tr>';
 	}
 	table += '</tbody>';
 
 	document.getElementById('listTable').innerHTML = table;
+}
+
+//formatando o nome do produto
+function formatDesc(desc){
+	var str = desc.toLowerCase();
+	str = str.charAt(0).toUpperCase() + str.slice(1);
+	return str;
+}
+
+//formatando o preço
+function formatValue(value){
+	var str = parseFloat(value).toFixed(2) + "";
+	str = str.replace(".",",");
+	str = "$ " + str;
+	return str;
 }
 
 setList(list);
