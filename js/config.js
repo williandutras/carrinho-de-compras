d1b1447b8deba2aed1fcbe1fcb0235e509fb0982
Willian Dutras <willian_dutras@hotmail.com>
2017-06-23 15:22:02 -0300

    Configurando a aplicação

---
+++ js/config.js
@@ -0,0 +1,9 @@
+function setConfig(){
+	var texts = {
+		"title":"Carrinho de Compras"
+	};
+	document.title = texts.title;
+	document.getElementById("navTitle").innerHTML = texts.title;
+}
+
+setConfig();
