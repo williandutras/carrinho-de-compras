96f9c669cf1c8362335602c607033d56a1dd1edc
Willian Dutras <willian_dutras@hotmail.com>
2017-06-23 16:28:25 -0300

    Atualizando dados

--- js/main.js
+++ js/main.js
@@ -56,6 +56,8 @@ function setUpdate(id){
 	document.getElementById("value").value = obj.value;
 	document.getElementById("btnUpdate").style.display = "inline-block";
 	document.getElementById("btnAdd").style.display = "none";
+
+	document.getElementById("inputIDUpdate").innerHTML = '<input id="idUpdate" type="hidden" value="'+id+'">';
 }
 
 //limpa os campos de editar
@@ -65,6 +67,19 @@ function resetForm(){
 	document.getElementById("value").value = "";
 	document.getElementById("btnUpdate").style.display = "none";
 	document.getElementById("btnAdd").style.display = "inline-block";
+	document.getElementById("inputIDUpdate").innerHTML = "";
+}
+
+//atualizando os dados
+function updateData(){
+	var id = document.getElementById("idUpdate").value;
+	var desc = document.getElementById("desc").value;
+	var amount = document.getElementById("amount").value;
+	var value = document.getElementById("value").value;
+
+	list[id] = {"desc":desc, "amount":amount, "value":value};
+	resetForm();
+	setList(list);
 }
 
 setList(list);
