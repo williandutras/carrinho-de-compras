fbed31227c15ed9162e9a56e082bf78e8400d7dd
Willian Dutras <willian_dutras@hotmail.com>
2017-06-23 16:19:42 -0300

    Preparando para atualizar registros

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function getTotal(list){
 function setList(list){
 	var table = '<thead><tr><td>Description</td><td>Amount</td><td>Value</td><td>Action</td></tr></thead><tbody>';
 	for(var key in list){
-		table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td>Edit | Delete</td></tr>';
+		table += '<tr><td>'+ formatDesc(list[key].desc) +'</td><td>'+ list[key].amount +'</td><td>'+ formatValue(list[key].value) +'</td><td><button class="btn btn-default" onclick="setUpdate('+key+');">Edit</button> | Delete</td></tr>';
 	}
 	table += '</tbody>';
 
@@ -46,6 +46,25 @@ function addData(){
 
 	list.unshift({"desc":desc , "amount":amount , "value":value });
 	setList(list);
+}
+
+//botões de editar
+function setUpdate(id){
+	var obj = list[id];
+	document.getElementById("desc").value = obj.desc;
+	document.getElementById("amount").value = obj.amount;
+	document.getElementById("value").value = obj.value;
+	document.getElementById("btnUpdate").style.display = "inline-block";
+	document.getElementById("btnAdd").style.display = "none";
+}
+
+//limpa os campos de editar
+function resetForm(){
+	document.getElementById("desc").value = "";
+	document.getElementById("amount").value = "";
+	document.getElementById("value").value = "";
+	document.getElementById("btnUpdate").style.display = "none";
+	document.getElementById("btnAdd").style.display = "inline-block";
 }
 
 setList(list);
